e010af331fee474e809d26cdd4b819494288dbc6
SamTheDevDE <blvcksatan187@gmail.com>
2024-10-29 17:15:54 +0100

    changed some dashboard settings and made it so it auto starts in the index.ts file

--- src/dashboard/app.js
+++ src/dashboard/app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const app = express();
+export default function startDashboard() {
+	const express = require('express');
+	const app = express();
+	let port = require('./config.json').port || 3000;
+	const { Logger } = require("#lib/structures")
+	const logger = new Logger();
+	logger.info(`Starting dashboard...`);
+	app.set('port', port);
+	const session = require('express-session');
+	app.set('view engine', 'ejs');
+	app.use(express.static('static'));
+	app.use(session({
+	    secret: '48738924783748273742398747238',
+	    resave: false,
+	    saveUninitialized: false,
+	    expires: 604800000,
+	}));
+	require('./router')(app);
+	app.listen(port, () => logger.info(`Dashboard listening on port ${port}`));
+}
 
-let port = require('./config.json').port || 3000;
-app.set('port', port);
 
-const session = require('express-session');
-
-app.set('view engine', 'ejs');
-app.use(express.static('static'));
-app.use(session({
-    secret: '48738924783748273742398747238',
-    resave: false,
-    saveUninitialized: false,
-    expires: 604800000,
-}));
-require('./router')(app);
-
-app.listen(port, () => console.info(`Listening on port ${port}`));
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Client, connectMySQL } from '#lib/structures';
 import { startAPI } from '#root/api/api'
+import startDashboard from "#root/dashboard/app"
 const client = new Client();
 
 await connectMySQL();
 await startAPI();
+await startDashboard();
 await client.login();
